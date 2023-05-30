63eed5883b1db15556cc5b11b59be42362af8eca
elordemo <emresenkan98@gmail.com>
2023-05-30 13:31:52 +0300

    HarfNotu
    C# Alınan nota göre harf notu belirlemek.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿// C# programlamada öğrecinin sınavdan aldığı nota göre harf notu belirleme.
+using System;
+
+namespace Notlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int not = Int32.Parse(Console.ReadLine());
+            if (not > 100)
+            {
+                Console.WriteLine("Lütfen geçerli not giriniz");
+            }
+            else if(not > 80 && not < 100) 
+            {
+                Console.WriteLine("Öğrencinin Harf Notu: AA");
+            }
+            else if(not > 60 && not < 80)
+            {
+                Console.WriteLine("Öğrencinin Harf Notu: BB");
+            }
+            else if (not > 50 && not < 60)
+            {
+                Console.WriteLine("Öğrencinin Harf Notu: CC");
+            }
+              else 
+            {
+                Console.WriteLine("Öğrenci başarısız");
+            }
+        }
+    }
+}
